05655d39a03ba5286a4214d3afd31c5010e0cf83
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2023-12-11 17:18:49 +0100

    Create LookupSprite.cs

---
+++ Engine/src/Pyrite/Core/Graphics/LookupSprite.cs
@@ -0,0 +1,16 @@
+﻿namespace Pyrite.Core.Graphics
+{
+    internal class LookupSprite
+    {
+        Texture2D Source;
+        Texture2D Lookup;
+        Texture2D? Remap = null;
+
+        public LookupSprite(Texture2D source, Texture2D lookup, Texture2D? remap = null)
+        {
+            Source = source;
+            Lookup = lookup;
+            Remap = remap;
+        }
+    }
+}
