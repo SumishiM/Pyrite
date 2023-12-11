707d49402683fa9223a46aff847bd71527aa6dbc
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2023-12-11 14:05:27 +0100

    PlayerInput class

---
+++ Engine/src/Pyrite/Core/Inputs/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyrite.Core.Inputs
+{
+    public class PlayerInput : Component
+    {
+        public InputContextMapping? IMC;
+    }
+}
