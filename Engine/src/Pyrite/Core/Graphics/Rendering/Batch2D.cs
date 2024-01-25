f928c36e92b705967f641d5609f20580ae404767
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2024-01-25 14:32:27 +0100

    Update Batch2D.cs

--- Engine/src/Pyrite/Core/Graphics/Rendering/Batch2D.cs
+++ Engine/src/Pyrite/Core/Graphics/Rendering/Batch2D.cs
@@ -1,8 +1,26 @@
-﻿namespace Pyrite.Core.Graphics.Rendering
+﻿using Pyrite.Core.Graphics.Shaders;
+using Silk.NET.Vulkan;
+
+namespace Pyrite.Core.Graphics.Rendering
 {
     public class Batch2D
     {
         List<Sprite> Sprites;
+
+        Dictionary<int, Shader> Shaders;
+        Dictionary<int, Texture> Textures;
+
+        public struct RenderData
+        {
+            int Shader;
+            int Texture;
+            Transform Transform;
+        }
+
+        public Dictionary<int, HashSet<int>> ShaderToTextures;
+        public Dictionary<int, RenderData> RenderDatas;
+
+
         public Batch2D()
         {
             Sprites = new List<Sprite>();
