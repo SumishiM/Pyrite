6d1d9ea312244e0fc4067f65aa577441803acef6
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2024-01-31 10:46:20 +0100

    Update spin

--- Sandbox/SpinSystem.cs
+++ Sandbox/SpinSystem.cs
@@ -3,7 +3,6 @@ using Ignite.Systems;
 using Pyrite.Core.Components;
 using Pyrite.Core.Graphics;
 using Pyrite.Utils;
-using System.Numerics;
 
 namespace Sandbox
 {
@@ -15,10 +14,9 @@ namespace Sandbox
             foreach ( var node in context.Nodes )
             {
                 Sprite sprite = node.GetComponent<SpriteComponent>();
-                sprite.Transform.Rotation += 720f * MathF.Cos(Time.TotalTime) * Time.DeltaTime;
-                sprite.Transform.Position = 
-                    Vector2.UnitX * 360f * MathF.Cos(Time.TotalTime * 5f) + 
-                    Vector2.UnitY * 180f * MathF.Sin(Time.TotalTime * 5f);
+
+                sprite.Transform.Rotation += 
+                    node.GetComponent<SpinComponent>().SpinSpeed * Time.DeltaTime;
             }
         }
 
