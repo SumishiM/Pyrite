b8ac84a2465d4cb8e7f8dbdd7af4bbfe8fa44ac1
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2024-03-01 16:16:13 +0100

    cleaning

--- Sandbox/SpinSystem.cs
+++ Sandbox/SpinSystem.cs
@@ -1,6 +1,5 @@
 ﻿using Ignite.Attributes;
 using Ignite.Systems;
-using Pyrite.Attributes;
 using Pyrite.Components;
 using Pyrite.Core;
 using Pyrite.Utils;
@@ -10,29 +9,13 @@ namespace Sandbox
     [FilterComponent(typeof(SpinComponent))]
     public struct SpinSystem : IFixedUpdateSystem
     {
-        public void FixedUpdate( Context context )
+        public readonly void FixedUpdate( Context context )
         {
             foreach ( var node in context.Nodes )
             {
                 Transform transform = node.GetComponent<TransformComponent>();
                 transform.Rotation += node.GetComponent<SpinComponent>().SpinSpeed * Time.FixedDeltaTime;
             }
-        }
-
-        public void Dispose ()
-        {
-        }
-    }
-
-    [PercistentSystem]
-    public struct PermaSystem : IStartSystem
-    {
-        public void Dispose()
-        {
-        }
-
-        public void Start(Context context)
-        {
         }
     }
 }
